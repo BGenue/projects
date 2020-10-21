97b0ac4ca266b2b52319db1e9d9b7b62c015d3dc
BGenue <qo9895@hanmail.net>
2020-10-21 13:26:00 +0900

    수정, 삭제 기능 추가

--- scubawithgnb/app.py
+++ scubawithgnb/app.py
@@ -25,15 +25,17 @@ def write_community():
     community_name = request.form['name']
     community_password = request.form['password']
     community_text = request.form['text']
-    community_noshow = request.form['noshow'];
+    community_secrete = request.form['secrete'];
     community_title = request.form['title'];
+    community_id = request.form['id'];
     print(community_text)
     doc = {
         'community_name': community_name,
         'community_password': community_password,
         'community_text': community_text,
         'community_title': community_title,
-        'community_noshow': community_noshow
+        'community_secrete': community_secrete,
+        'community_id': community_id
     }
 
     db.community.insert_one(doc)
@@ -46,6 +48,40 @@ def read_community():
     communities = list(db.community.find({}, {'_id': False}))
 
     return jsonify({'result': 'success', 'communities': communities})
+
+#게시판 글 지워
+@app.route('/community-form/del', methods=['POST'])
+def delete_community():
+    delete_id = request.form['id']
+    delete = {
+        'community_id': delete_id
+    }
+    db.community.delete_one(delete)
+
+    return jsonify({'result': 'success', 'msg': '글이 삭제되었습니다.'})
+
+#게시판 글 수정
+@app.route('/community-form/fix', methods=['POST'])
+def fix_community():
+    fix_name = request.form['name']
+    fix_password = request.form['password']
+    fix_text = request.form['text']
+    fix_secrete = request.form['secrete'];
+    fix_title = request.form['title'];
+    fix_id = request.form['id'];
+    print(fix_name, fix_password, fix_text, fix_secrete, fix_title, fix_id)
+    fix = {
+        'community_name': fix_name,
+        'community_password': fix_password,
+        'community_text': fix_text,
+        'community_title': fix_title,
+        'community_secrete': fix_secrete,
+        'community_id': fix_id,
+    }
+
+    db.community.update_one({'community_id': fix_id}, {'$set': fix})
+
+    return jsonify({'result': 'success', 'msg': '글이 수정되었습니다.'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
